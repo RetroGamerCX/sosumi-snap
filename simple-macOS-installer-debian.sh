0fac6ec1db37bf2a3002a93c28a7edb76d8e33df
retro 64K <65816689+retro-64K@users.noreply.github.com>
2021-06-05 20:16:38 -0300

    Update simple-macOS-installer-debian.sh

--- simple-macOS-installer-debian.sh
+++ simple-macOS-installer-debian.sh
@@ -10,16 +10,16 @@ sudo apt upgrade --yes
 sudo apt install qemu-kvm qemu-system-x86 qemu-utils --yes
 mkdir ~/simple-macOS/firmware
 cd ~/simple-macOS/firmware
-wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/firmware/OVMF_CODE.fd?raw=true
+wget https://github.com/retro-64K/simple-macOS/blob/master/snap/local/firmware/OVMF_CODE.fd?raw=true
 mv 'OVMF_CODE.fd?raw=true' 'OVMF_CODE.fd'
-wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/firmware/OVMF_VARS-1024x768.fd?raw=true
+wget https://github.com/retro-64K/simple-macOS/blob/master/snap/local/firmware/OVMF_VARS-1024x768.fd?raw=true
 mv 'OVMF_VARS-1024x768.fd?raw=true' OVMF_VARS-1024x768.fd
 cd ~/simple-macOS
-wget https://github.com/RetroGamerCX/simple-macOS/blob/master/snap/local/ESP.qcow2?raw=true
+wget https://github.com/retro-64K/simple-macOS/blob/master/snap/local/ESP.qcow2?raw=true
 mv 'ESP.qcow2?raw=true' ESP.qcow2
 sudo apt install dmg2img --yes
 dmg2img -v -i BaseSystem.dmg BaseSystem.img
-wget https://raw.githubusercontent.com/RetroGamerCX/simple-macOS/master/snap/local/bin/launch
+wget https://raw.githubusercontent.com/retro-64K/simple-macOS/master/snap/local/bin/launch
 sudo chmod +x launch
 qemu-img create -f raw ~/simple-macOS/macOS.img 256G
 clear
